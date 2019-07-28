52d3983d1d2a3a1434deb5a38a2983d0ed6abbc1
itning <itning@itning.top>
2019-07-28 12:50:33 +0800

    update

---
+++ algorithm/bubble_sort.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	data := []int{6, 7, 3, 5, 1, 2, 4}
+	BubbleSort(data)
+	fmt.Println(data)
+}
+
+func BubbleSort(data []int) {
+	n := len(data)
+	for i := 0; i < n-1; i++ {
+		isChanged := false
+		for j := 0; j < n-1-i; j++ {
+			if data[j] < data[j+1] {
+				data[j], data[j+1] = data[j+1], data[j]
+				isChanged = true
+			}
+		}
+		if !isChanged {
+			break
+		}
+	}
+}
