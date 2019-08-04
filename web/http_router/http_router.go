46896f166c86042c692edec15a56d6da6d38967c
itning <itning@itning.top>
2019-08-04 17:04:35 +0800

    update

--- web/http_router/http_router.go
+++ web/http_router/http_router.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
+
+type User struct {
+	Id   int
+	Name string
+	Pwd  string
+}
 
 func main() {
 	router := httprouter.New()
@@ -16,7 +23,18 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	_, err := fmt.Fprint(w, "Welcome!\n")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	user := User{Id: 1, Name: "哈哈", Pwd: "123"}
+	bytes, e := json.Marshal(user)
+	if e != nil {
+		_, e := w.Write([]byte("出错"))
+		if e != nil {
+			log.Print(e.Error())
+		}
+		return
+	}
+	_, err := w.Write(bytes)
+	//_, err := fmt.Fprint(w, "Welcome!\n")
 	if err != nil {
 		log.Print(err.Error())
 	}
